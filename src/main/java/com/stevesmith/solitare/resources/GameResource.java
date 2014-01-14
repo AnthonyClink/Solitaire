9922c3873c0141160d545fe836e19b6a10e3110f
AnthonyClink <AnthonyJClink@gmail.com>
2014-01-13 19:24:23 -0800

    added ability to have multiple game boards, started drafting the move url

--- src/main/java/com/stevesmith/solitare/resources/GameResource.java
+++ src/main/java/com/stevesmith/solitare/resources/GameResource.java
@@ -1,20 +1,19 @@
 package com.stevesmith.solitare.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.log4j.Logger;
+
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.stevesmith.solitaire.datatype.Card;
 import com.stevesmith.solitaire.datatype.Deck;
 import com.stevesmith.solitaire.datatype.GameBoard;
-import com.stevesmith.solitaire.datatype.Rank;
-import com.stevesmith.solitaire.datatype.Suit;
+import com.stevesmith.solitaire.datatype.GameSpot;
 import com.stevesmith.solitaire.domain.GameService;
 
 @Singleton
@@ -22,6 +21,7 @@ import com.stevesmith.solitaire.domain.GameService;
 @Produces(MediaType.APPLICATION_JSON)
 public class GameResource {
 
+	private static final Logger LOGGER = Logger.getLogger(GameResource.class);
 	private GameService gameService;
 
 	
@@ -32,12 +32,25 @@ public class GameResource {
 	}
     
     @GET
-    @Path("gameboard")
+    @Path("gameboard/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public GameBoard getGameBoard(){
+    public GameBoard getGameBoard(@PathParam("id") String id){
+    	info("Sending gameboard resource: " + id + " to client");
     	gameService.newGame();
     	return gameService.getGameBoard();
-    	
+    }
+    
+    @PUT
+    @Path("gameboard/{id}/pile/{pileId}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Deck getPile(@PathParam("id") String gameId, @PathParam("pileId") String pileId){
+    	info("getting " + pileId);
+    	return gameService.getGameBoard().getGameSpot(GameSpot.valueOf(pileId));
+    }
+    
+    private void info(String message){
+    	//LOGGER.info(message);
+    	System.out.println(message);
     }
     
 }
