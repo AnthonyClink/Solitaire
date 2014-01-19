5f96d5932b521c2650b43c73cc92747150062cf5
Andrew Spiering <aspiering@gmail.com>
2014-01-18 21:50:26 -0800

    Test

--- src/main/java/com/stevesmith/solitaire/Solitaire.java
+++ src/main/java/com/stevesmith/solitaire/Solitaire.java
@@ -23,7 +23,7 @@ public class Solitaire {
 		
 		Server server = new Server(8080);
 		
-		//added some comments to see if the Jenkins build server will pick up the git hub changes
+		//added some comments to see if the Jenkins build server will pick up the git hub changes 
 		
 		ServletContextHandler handler = new ServletContextHandler();
 		handler.setContextPath("/");
